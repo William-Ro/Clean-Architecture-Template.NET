1f8c81954a2c428b743aece0c4f82347afaf7ae4
William-Ro <william.r2409@gmail.com>
2025-03-26 14:21:30 -0600

    chore: create dev-shell for my project

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dev Shell for .NET Development";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux"; # or "aarch64-linux" for ARM
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.dotnet-sdk_8
+        pkgs.dotnet-ef
+        pkgs.jetbrains.rider # Optional
+      ];
+
+      shellHook = ''
+        echo "✅ .NET Dev Shell Activated!"
+        export DOTNET_CLI_TELEMETRY_OPTOUT=1
+      '';
+    };
+  };
+}
